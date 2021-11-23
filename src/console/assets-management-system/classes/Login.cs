65bc15a67021026d8822b2a79a0f69be441dd3da
phungthanhtu <19522455@gm.uit.edu.vn>
2021-11-23 07:07:17 +0700

    add login module

---
+++ src/console/assets-management-system/classes/Login.cs
@@ -0,0 +1,19 @@
+namespace assets_management_system {
+    class User {
+        string Username = "";
+        string Password = "";
+
+        public User(){}
+        public void input(string username, string password)
+        {
+
+        }
+        public bool accessSystem()
+        {
+            return true;
+        }
+    }
+
+
+
+}
