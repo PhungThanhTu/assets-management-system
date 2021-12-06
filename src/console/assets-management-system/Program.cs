363752e0c6929f70e91f079be4a55a4d4d81b8ec
phungthanhtu <19522455@gm.uit.edu.vn>
2021-12-06 08:10:37 +0700

    re setup login classes

--- src/console/assets-management-system/Program.cs
+++ src/console/assets-management-system/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace assets_management_system
 {
     class Program
-    {
-        static void Main(string[] args)
+    {   
+        private static readonly HttpClient client = new HttpClient();
+
+       
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             await ProcessRepositories();
+        }
+
+        private static async Task ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = client.GetStringAsync("http://localhost:3000/");
+
+            var msg = await stringTask;
+            Console.Write(msg);
         }
     }
 }
--- src/console/assets-management-system/classes/Login.cs
+++ src/console/assets-management-system/classes/Login.cs
@@ -1,19 +1,7 @@
 namespace assets_management_system {
     class User {
-        string Username = "";
-        string Password = "";
-
-        public User(){}
-        public void input(string username, string password)
-        {
-
-        }
-        public bool accessSystem()
-        {
-            return true;
-        }
+        public string Name {get;set;}
+        public string Password{get;set;}
     }
-
-
 
 }
