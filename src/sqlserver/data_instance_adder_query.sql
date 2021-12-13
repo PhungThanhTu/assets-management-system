c00f74fc8751368b915d21da4003bf1d6d5cfa71
UIT19520358 <19520358@gm.uit.edu.vn>
2021-12-14 06:49:31 +0700

    fix lan 1

---
+++ src/sqlserver/data_instance_adder_query.sql
@@ -0,0 +1,38 @@
+﻿use AssetsManagement
+go
+
+-- Supplier --
+INSERT INTO Supplier (name) VALUES (N'Thế giới di động')
+INSERT INTO Supplier (name) VALUES (N'FPT')
+INSERT INTO Supplier (name) VALUES (N'Tin học ngôi sao')
+INSERT INTO Supplier (name) VALUES (N'GearVN')
+-- Device_Set --
+INSERT INTO Device_Set (name) VALUES (N'PC văn phòng')
+INSERT INTO Device_Set (name) VALUES (N'PC ASUS ROG Strix')
+-- Contracts --
+SET IDENTITY_INSERT Contracts ON
+INSERT INTO Contracts (id) VALUES (1)
+INSERT INTO Contracts (id) VALUES (2)
+-- Device --
+INSERT INTO Device (contract,set_id,name,price) VALUES (1,1,N'Màn hình samsung',750000)
+INSERT INTO Device (contract,set_id,name,price) VALUES (1,1,N'CPU intel pentium 4',350000)
+INSERT INTO Device (contract,set_id,name,price) VALUES (1,1,N'Chuột OEM',70000)
+INSERT INTO Device (contract,set_id,name,price) VALUES (1,1,N'Bàn phím OEM',135000)
+INSERT INTO Device (contract,set_id,name,price) VALUES (1,1,N'Case',700000)
+INSERT INTO Device (contract,set_id,name,price) VALUES (1,1,N'Nguồn',300000)
+INSERT INTO Device (contract,set_id,name,price) VALUES (1,1,N'Main',300000)
+
+INSERT INTO Device (contract,set_id,name,price) VALUES (2,2,N'Case Asus Rog Strix Helios GX 601',7900000)
+INSERT INTO Device (contract,set_id,name,price) VALUES (2,2,N'Main Asus Rog Strix B365-G GAMING',2000000)
+INSERT INTO Device (contract,set_id,name,price) VALUES (2,2,N'Nguồn Asus Rog Strix 650W',3800000)
+INSERT INTO Device (contract,set_id,name,price) VALUES (2,2,N'GPU RTX 3060Ti',22000000)
+INSERT INTO Device (contract,set_id,name,price) VALUES (2,2,N'CPU Intel core i5 11400F',6000000)
+INSERT INTO Device (contract,set_id,name,price) VALUES (2,2,N'RAM Samsung DDR4 16GB',2000000)
+INSERT INTO Device (contract,set_id,name,price) VALUES (2,2,N'SSD Samsung EVO 970 Plus',3100000)
+INSERT INTO Device (contract,set_id,name,price) VALUES (2,2,N'Quạt tản nhiệt CPU cooler master t40',300000)
+INSERT INTO Device (contract,set_id,name,price) VALUES (2,2,N'Quạt tản nhiệt CPU cooler master t40',300000)
+INSERT INTO Device (contract,set_id,name,price) VALUES (2,2,N'Quạt tản nhiệt CPU cooler master t40',300000)
+INSERT INTO Device (contract,set_id,name,price) VALUES (2,2,N'Bàn phím AKKO 3087 vs DS Midnight',1200000)
+INSERT INTO Device (contract,set_id,name,price) VALUES (2,2,N'Chuột Microsoft Arc Mouse',2400000)
+INSERT INTO Device (contract,set_id,name,price) VALUES (2,2,N'Màn hình Asus ROG Strix XG27WQ 2K 165Hz',12000000)
+INSERT INTO Device (contract,set_id,name,price) VALUES (2,2,N'Dàn loa Sony 5.1 HT-S20R 400W',4000000)
--- src/sqlserver/initializing_query.sql
+++ src/sqlserver/initializing_query.sql
@@ -22,8 +22,8 @@ go
 create table Supplier
 (
 	id int identity(1,1) primary key,
-	name varchar(50),
-	address varchar(50),
+	name nvarchar(50),
+	address nvarchar(50),
 	phone varchar(50)
 )
 go
@@ -47,16 +47,16 @@ go
 create table Division
 (
 	id int identity(1,1) primary key,
-	name varchar(50),
-	type varchar(20)
+	name nvarchar(50),
+	type nvarchar(20)
 )
 go
 
 create table Contracts
 (
 	id int identity(1,1) primary key,
-	status varchar(10),
-	supplier int not null
+	status nvarchar(10),
+	supplier int
 
 	foreign key (supplier) references Supplier(id)
 )
@@ -65,11 +65,18 @@ go
 create table Personnel
 (
 	id int identity(1,1) primary key,
-	name varchar(20),
+	name nvarchar(20),
 	position varchar(20),
 	division int not null
 
 	foreign key (division) references Division(id)
+)
+go
+
+create table Device_Set
+(
+	id int identity(1,1) primary key,
+	name nvarchar(20)
 )
 go
 
@@ -84,13 +91,141 @@ create table Device
 	status nvarchar(50),
 	annual_value_lost float,
 	contract int not null,
-	holding_division int not null,
-	unit int not null,
-	type int not null
+	holding_division int,
+	unit int,
+	type int,
+	set_id int not null,
+	current_value int
 
 	foreign key (contract) references Contracts(id),
 	foreign key (holding_division) references Division(id),
 	foreign key (unit) references Unit(id),
-	foreign key (type) references Types(id)
+	foreign key (type) references Types(id),
+	foreign key (set_id) references Device_Set(id)
+)
+go
+
+create table Inventory
+(
+	id int identity(1,1) primary key,
+	date date
+)
+go
+
+create table Detailed_Inventory
+(
+	remaining_value int,
+	inventory int,
+	device int
+
+	foreign key (inventory) references Inventory(id),
+	foreign key (device) references Device(id),
+	primary key (inventory,device)
+)
+go
+
+create table Detailed_Inventory_Personnel
+(
+	inventory int,
+	personnel int
+
+	foreign key (inventory) references Inventory(id),
+	foreign key (personnel) references Personnel(id),
+	primary key (inventory,personnel)
+)
+go
+
+create table Liquidation
+(
+	id int identity(1,1) primary key,
+	date date
+)
+go
+
+create table Detailed_Liquidation
+(
+	liquidation int,
+	device int
+
+	foreign key (liquidation) references Liquidation(id),
+	foreign key (device) references Device(id),
+	primary key (liquidation,device)
+)
+go
+
+create table Detailed_Liquidation_Personnel
+(
+	liquidation int,
+	personnel int
+
+	foreign key (liquidation) references Liquidation(id),
+	foreign key (personnel) references Personnel(id),
+	primary key (liquidation,personnel)
+)
+go
+
+create table Transfers
+(
+	id int identity(1,1) primary key,
+	sender int,
+	receiver int
+
+	foreign key (sender) references Division(id),
+	foreign key (receiver) references Division(id)
+)
+go
+
+create table Detailed_Transfers
+(
+	transfers int,
+	device int
+
+	foreign key (transfers) references Transfers(id),
+	foreign key (device) references Device(id),
+	primary key (transfers,device)
+)
+go
+
+create table Fix
+(
+	id int identity(1,1) primary key,
+	decide_id int,
+	date date,
+	repairer int,
+	total real null
+)
+go
+
+create table Detailed_Fix
+(
+	fix int,
+	device int,
+	fee real
+
+	foreign key (fix) references Fix(id),
+	foreign key (device) references Device(id),
+	primary key (fix,device)
+)
+go
+
+Select Sum(fee) as "Tong chi phi" FROM Detailed_Fix GROUP BY fix
+ALTER TABLE Fix
+ADD CONSTRAINT CHK_Total CHECK (total = 'Tong chi phi')
+
+create table Worth
+(
+	id int identity(1,1) primary key,
+	date date
+)
+
+create table Deatiled_Worth
+(
+	worth int,
+	device int,
+	status nvarchar(50)
+
+	foreign key (worth) references Worth(id),
+	foreign key (device) references Device(id),
+	primary key (worth,device)
 )
 go
