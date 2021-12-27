00a5078de9255ae06bce2ba223f7ebcb22c2ed86
ThanhTuan <72434152+thanhtuan2503-uit@users.noreply.github.com>
2021-12-27 09:28:51 +0700

    mainwindow

---
+++ src/wpf/assets-management-system/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using assets_management_system.Page;
+using assets_management_system.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace assets_management_system
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            //LoginWindow loginWindow = new LoginWindow();
+            //loginWindow.ShowDialog();
+            ////MainWindow mainWindow = new MainWindow();
+            ////mainWindow.Show();
+        }
+       
+
+        
+
+        
+        
+     
+        private void LogOutClicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void Account_Click(object sender, RoutedEventArgs e)
+        {
+           PagesNavigation.Navigate(new AccountPage());
+        }
+
+        private void Device_Click(object sender, RoutedEventArgs e)
+        {
+            PagesNavigation.Navigate(new DevicePage());
+        }
+
+
+        private void DeviceDetail_Click(object sender, RoutedEventArgs e)
+        {
+            PagesNavigation.Navigate(new DeviceDetailPage());
+        }
+
+        private void Repair_Click(object sender, RoutedEventArgs e)
+        {
+            PagesNavigation.Navigate(new RepairPage());
+        }
+    }
+}
