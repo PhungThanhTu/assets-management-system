7ef2a397d01ededa482a07528f9189d47511ecfe
phungthanhtu <19522455@gm.uit.edu.vn>
2021-12-30 12:40:01 +0700

    ignore .vs and changed domain name
    - change domain name for webserver
    - ignore all .vs folder

--- src/wpf/assets-management-system/API_config.cs
+++ src/wpf/assets-management-system/API_config.cs
@@ -8,6 +8,6 @@ namespace assets_management_system
 {
     static class API_config
     {
-        public const string enpoint_uri = "https://evening-mountain-03563.herokuapp.com/";
+        public const string enpoint_uri = "https://asset-management-webserver.herokuapp.com/";
     }
 }
